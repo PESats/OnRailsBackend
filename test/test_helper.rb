06dc6138fd6681db3150f1f1ec28ac583375ea5a
Marti Lloveras <marti-nini@hotmail.com>
2017-10-20 16:04:52 +0200

    Add correct_user for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,14 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  
+  def correct_user
+    User.new(name: "Lone Wolf", platform_name: "Google", email: "dude@example.com", active_token: "1234")
+  end
+  
 end
