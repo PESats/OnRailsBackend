be66fbadda669e1cf8c11c845753aa739e4fda30
Marti Lloveras <marti-nini@hotmail.com>
2017-10-20 12:38:13 +0200

    Backend migration

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  
+ resources :users
+ 
+ #get 'users/index'
+
+ #get 'users/new'
+
+ #get 'users/create'
+
+ #get 'users/show'
+
+ #get 'users/edit'
+
+ #get 'users/update'
+
+ #get 'users/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
