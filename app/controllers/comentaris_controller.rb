5ed37b2c86f562059c0472f343986563799b9025
Daniel Garrapucho <dangarrapucho@gmail.com>
2017-11-07 12:18:44 +0100

    canvis menors al controlador de comentaris

--- app/controllers/comentaris_controller.rb
+++ app/controllers/comentaris_controller.rb
@@ -1,9 +1,18 @@
 class ComentarisController < ApplicationController
-  
+
   def index
   end
 
   def create
+    @anunci = Anunci.find(params[:anunci_id])
+    @user = User.find(params[:user_id])
+    @comentari = @anunci.comentaris.create(comentari_params)
+    if @comentari.save
+      @user.comentaris << @comentari
+      render json: @comentari
+    else
+      render json: @comentari.errors.full_message, status: 400
+    end
   end
 
   def show
@@ -14,8 +23,12 @@ class ComentarisController < ApplicationController
 
   def destroy
   end
-  
+
   private
 ################################################################################
-  
+  def commentari_params
+    params.require(:comentari).permit(:text, :user_id, :anunci_id)
+  end
+
+
 end
