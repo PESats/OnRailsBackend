28bc51c5a6c4b2d32313db61fc2931c78b75c57d
Marti Lloveras <marti-nini@hotmail.com>
2017-11-28 13:16:28 +0100

    Add Shop model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116195445) do
+ActiveRecord::Schema.define(version: 20171128121153) do
 
   create_table "anuncis", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20171116195445) do
     t.integer "reward", default: 0
     t.index ["anunci_id"], name: "index_comentaris_on_anunci_id"
     t.index ["user_id"], name: "index_comentaris_on_user_id"
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
