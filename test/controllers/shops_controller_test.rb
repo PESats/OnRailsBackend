89c2b12dde3a4f895104de12014f25600bb6a14f
Marti Lloveras <marti-nini@hotmail.com>
2017-12-01 15:04:58 +0100

    :rotating_light: Add shop create test

--- test/controllers/shops_controller_test.rb
+++ test/controllers/shops_controller_test.rb
@@ -29,4 +29,34 @@ class ShopsControllerTest < ActionDispatch::IntegrationTest
     assert_equal "200", response.code
   end
 
+  test "create shop" do
+    user = correct_user
+    user.login
+    user.reload
+
+    name = "Dummy name"
+    description = "Dummy description"
+    latitude = 2.7
+    longitude = 1.5
+
+    post shops_path, params: {
+      shop: {
+        name: name,
+        description: description,
+        latitude: latitude,
+        longitude: longitude
+      },
+      user_id: user.id, active_token: user.active_token
+    }
+
+    shop = user.reload.shop
+
+    assert_equal "200",       response.code
+    assert_equal name,        anunci.name
+    assert_equal description, anunci.description
+    assert_equal latitude,    anunci.latitude
+    assert_equal longitude,   anunci.longitude
+
+  end
+
 end
