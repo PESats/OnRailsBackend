cea447c6a7dc009317148188906be38ac5290452
Marti Lloveras <marti-nini@hotmail.com>
2017-12-01 13:02:25 +0100

    Add user reference to shop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128125305) do
+ActiveRecord::Schema.define(version: 20171201115013) do
 
   create_table "anuncis", force: :cascade do |t|
     t.string "title"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20171128125305) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
