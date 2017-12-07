4077b3280076ce4b53c8f6b906d53a19b539dec9
Marti Lloveras <marti-nini@hotmail.com>
2017-12-07 14:54:13 +0100

    :sparkles: User bought coupons get index

--- test/controllers/bought_coupons_controller_test.rb
+++ test/controllers/bought_coupons_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class BoughtCouponsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "get bought_coupons index" do
+    user = correct_user
+    user.login
+    
+    get user_bought_coupons_path(user), params: {
+      user_id: user.id,
+      active_token: user.active_token
+    }
+    
+    assert_equal "200",   response.code
+    user.bought_coupons.each do |coupon|
+      assert_match coupon.title,        response.body
+      assert_match coupon.description,  response.body
+      assert_match coupon.discount,     response.body
+      assert_match coupon.user_id,      response.body
+    end
+  end
+  
 end
