27b6853eb04dde8c5774002f459098b9c05c94e1
Marti Lloveras <marti-nini@hotmail.com>
2017-12-12 13:16:46 +0100

    :art:Refactoring seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,6 @@ n_anuncis_per_user = 5
 n_shops = 5
 n_coupons_per_shop = 1
 n_bids_per_user= 4
-#num_anun = 0
-#num_users= 0
-
 
 ################################################################################
 def randLatitude
@@ -20,7 +17,6 @@ def randLongitude
 end
 ################################################################################
 
-
 # Create users
 n_users.times do
   User.create name: Faker::Name.name, email: Faker::Internet.safe_email, platform_name: ["Facebook", "Google", "Twitter"].sample
@@ -28,19 +24,14 @@ end
 
 # Create anuncis per user
 User.all.each do |user|
-  #num_users += 1
-  #p("user #" +num_users.to_s+ " of "+User.all.count.to_s)
-  #num_anun = 0
   n_anuncis_per_user.times do
-    #num_anun += 1 
-    #p("anunci #"+num_anun.to_s+" of user #"+num_users.to_s)
-    anunci = user.anuncis.create( title: Faker::Job.title, 
-                          description: Faker::HarryPotter.quote, 
-                          latitude: randLatitude, 
-                          longitude: randLongitude, 
+    anunci = user.anuncis.create( title: Faker::Job.title,
+                          description: Faker::HarryPotter.quote,
+                          latitude: randLatitude,
+                          longitude: randLongitude,
                           reward: Faker::Number.number(1) )
-    anunci.comentaris.create text: Faker::FamilyGuy.quote, user_id: [user.id - 1, 1].max  
-  end   
+    anunci.comentaris.create text: Faker::FamilyGuy.quote, user_id: [user.id - 1, 1].max
+  end
 end
 
 User.all.each do |user|
@@ -51,7 +42,7 @@ User.all.each do |user|
     anunci = other_anuncis.sample
     #p("Anunci.id = "+anunci.id.to_s)
     if !anunci.bids.exists?(:user_id=>user.id)
-      user.bids.create(amount: Faker::Number.number(1),anunci_id: anunci.id)     
+      user.bids.create(amount: Faker::Number.number(1),anunci_id: anunci.id)
     end
   end
 end
@@ -65,11 +56,12 @@ end
 
 # Create shops
 n_shops.times do
-  Shop.create(  name: Faker::SiliconValley.company, 
-                description: Faker::SiliconValley.motto, 
-                latitude: randLatitude, 
-                longitude: randLongitude, 
-                user_id: User.first.id)
+  Shop.create(  name: Faker::SiliconValley.company,
+                description: Faker::SiliconValley.motto,
+                latitude: randLatitude,
+                longitude: randLongitude,
+                user_id: User.first.id
+  )
 end
 
 # Add coupons to shops
