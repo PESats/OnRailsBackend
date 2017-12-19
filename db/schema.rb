9aa044349aecf49b15904e3f6bffa71340d740d6
Marti Lloveras <marti-nini@hotmail.com>
2017-12-19 13:32:44 +0100

    :bug: Fix bought coupons shop_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211172516) do
+ActiveRecord::Schema.define(version: 20171219122850) do
 
   create_table "anuncis", force: :cascade do |t|
     t.string "title"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20171211172516) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "shop_id"
     t.index ["user_id"], name: "index_bought_coupons_on_user_id"
   end
 
